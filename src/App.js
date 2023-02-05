cabfcb69624d938438c6cda9dc9c46cb25d0bf0c
Fagner <frodriguesbetim@gmail.com>
2023-02-05 15:01:36 -0300

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+// in src/App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { fetchUtils, Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
+import { PostList, PostEdit, PostCreate } from './posts';
+import authProvider from './authProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      <Resource name="users" list={UserList} />
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+  </Admin>
+);
 
 export default App;
